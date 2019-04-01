5e08a80d5523e5114407d0866efe73d3992ad3db
Nikash Taskar <nikashtaskar@wustl.edu>
2019-03-31 23:10:52 -0500

    Messed around with data collection
    Wrote information to csv files, still a few kinks that need to be worked out.

--- py/dataCollect.py
+++ py/dataCollect.py
@@ -16,20 +16,31 @@ def createCSV(tour):
     filename = "../data/"+str(tour) + ".csv"
     ofile = open(filename, 'w')
 
-    tableItems = driver.find_elements_by_class_name("wikitable")
+    # tableItems = driver.find_elements_by_class_name("wikitable")
+    tableItems = driver.find_elements_by_tag_name("tr")
 
-    ofile.write("City,Date,Country,Venue,Attendance,Revenue")
+    # ofile.write("City,Date,Country,Venue,Attendance,Revenue")
+    ofile.write("City,Date,Country,Attendance,Revenue\n")
+
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
     for item in tableItems:
         line = item.text
-        line = line.split('\n')
-        # print(line)
-        i = 2
-        while i < len(line):
-            lineSplit = line[i].split(" ")
-            print(lineSplit)
-            i += 1
-            ofile.write(str(lineSplit))
+        for month in months:
+            splitLine = line.split(" ")
+            if month in line and 'N/A' not in line and len(splitLine) > 4:
+                # splitLine = line.split(" ")
+                dataLine = str(splitLine[3])+","+str(splitLine[0])+str(splitLine[1])+str(splitLine[2])+",United States,"+str(splitLine[len(splitLine)-4]).replace(",","")+","+str(splitLine[len(splitLine)-1]).replace(",","").replace("$","")+"\n"
+                ofile.write(dataLine)
+                print(dataLine)
+
+        # i = 2
+        # while i < len(line):
+        #     lineSplit = line[i].split(" ")
+        #     if len(lineSplit) > 2:
+        #         ofile.write(str(lineSplit))
+        #         print(lineSplit)
+        #     i += 1
 
         # ofile.write(item.text)
         # print(item.text)

15695391758d02914081d4beaec301ad4d3126a1
Nikash Taskar <nikashtaskar@wustl.edu>
2019-04-09 17:22:03 -0500

    Switch map format, add hover feature, beganbar chart work

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ var artists = ["weeknd", 'beyonce', "lamar", "bieber", "drake"];
 var tours = ["Starboy:_Legend_of_the_Fall_Tour", "The_Formation_World_Tour", "The_Damn_Tour", "Purpose_World_Tour", "Summer_Sixteen_Tour"];
 var defaultTour = tours[0];
 
+var textArea = d3.select("#text-area").append("svg")
+    .attr("id", "text-svg")
+    .attr("height", 20)
+    .attr("width", 200);
+
 var svg = d3.select("#chart-area").append("svg")
     .attr("id", "map-svg")
     .attr("height", height)
@@ -12,14 +17,17 @@ var svg = d3.select("#chart-area").append("svg")
 function loadData(tour) {
     queue()
         .defer(d3.json, "data/" + tour + ".json")
-        .defer(d3.json, "data/us-10m.json")
+        // .defer(d3.json, "data/us-10m.json")
+        .defer(d3.json, "data/us.json")
         .await(createVisualization);
 }
 
 function createVisualization(error, tourData, mapData) {
-        console.log(tourData);
+        // console.log(tourData);
 
         console.log(mapData);
+
+        // console.log(statesData);
 
         var projection = d3.geoAlbersUsa()
             .translate([width / 2, height / 2]);
@@ -27,7 +35,7 @@ function createVisualization(error, tourData, mapData) {
         var path = d3.geoPath()
             .projection(projection);
 
-        var usa = topojson.feature(mapData, mapData.objects.states).features;
+        var usa = topojson.feature(mapData, mapData.objects.usStates).features;
 
 
         svg.selectAll("path")
@@ -36,12 +44,24 @@ function createVisualization(error, tourData, mapData) {
             .append("path")
             .attr("d", path)
             .attr("class", "map")
-            .attr("stroke", "black")
-            .attr("fill", "lightgray");
+            .attr("stroke", "white")
+            .attr("stroke-width", "2px")
+            .attr("fill", "lightgray")
+            .attr("id", function(d) {
+                return d.properties.STATE_ABBR;
+            })
+            .on("mouseover", function(d) {
+                var stateName = d.properties.STATE_ABBR;
+                textArea.append("text")
+                    .text(stateName)
+                    .attr("x", 100)
+                    .attr("y", 20)
+                    .attr("class", "state-text");
+            })
+            .on("mouseout", function(d){
+                textArea.selectAll("text").remove();
+            })
 
-        svg.append("path")
-           .attr("class", "state-borders")
-           // .attr("d", path(topojson.mesh(mapData, mapData.objects.states, function(a, b) { return a !== b; })));
 
 }
 
@@ -63,9 +83,9 @@ function handleChange(event){
     var index = artists.indexOf(artist);
 
     var tour = tours[index];
-    console.log(tour);
 
     loadData(tour);
+
 }
 
 
